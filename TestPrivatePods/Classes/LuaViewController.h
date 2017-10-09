0dec3a94b83d261362eddac27044fca6e3cd3b28
luxiaol <lu_xiaol@126.com>
2017-10-09 11:04:07 +0800

    add test file

---
+++ TestPrivatePods/Classes/LuaViewController.h
@@ -0,0 +1,12 @@
+//
+//  LuaViewController.h
+//  Pods-TestPrivatePods_Example
+//
+//  Created by lu_xiaol on 2017/10/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LuaViewController : UIViewController
+
+@end
---
+++ TestPrivatePods/Classes/LuaViewController.m
@@ -0,0 +1,36 @@
+//
+//  LuaViewController.m
+//  Pods-TestPrivatePods_Example
+//
+//  Created by lu_xiaol on 2017/10/9.
+//
+
+#import "LuaViewController.h"
+
+@interface LuaViewController ()
+
+@end
+
+@implementation LuaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
